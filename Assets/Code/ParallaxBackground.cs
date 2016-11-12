c6d459d97a8c10dc6385ae2a43212b0b20b1de5b
Jon Pulsipher <jonapulse@gmail.com>
2016-11-12 17:06:16 -0500

    placed a lot of art and parallax background

---
+++ Assets/Code/ParallaxBackground.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxBackground : MonoBehaviour
+{
+	public float scrollFraction;
+
+	private Vector3 camStartPosition;
+	private Vector3 originalPosition;
+
+	void Start ()
+	{
+		camStartPosition = Camera.main.transform.position;
+		originalPosition = transform.position - Camera.main.transform.position;
+	}
+
+	void Update ()
+	{
+		transform.position = (Camera.main.transform.position - camStartPosition) * (1 - scrollFraction) + originalPosition;
+	}
+}
